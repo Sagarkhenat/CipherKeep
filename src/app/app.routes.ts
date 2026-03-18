f85581b7cf1aa7bc60ddcd0849843316a74fe1da
unknown <sgkhenat94@gmail.com>
2026-03-18 16:39:34 +0530

    implemented lockscreen UI -- added biometric-auth plugin authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,29 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth-guard';
 
 export const routes: Routes = [
   {
-    path: 'tabs',
-    loadChildren: () => import('./features/tabs/tabs.routes').then((m) => m.routes),
-  },
-  {
     path: '',
-    redirectTo: 'tabs',
+    redirectTo: 'lock-screen',
     pathMatch: 'full',
   },
+  {
+    path: 'lock-screen',
+    loadComponent: () => import('./features/lock-screen/lock-screen.component').then(m => m.LockScreenComponent)
+  },
+  {
+    path: 'tabs',
+    // We apply the guard here. This single line protects the Vault, Generator, and Settings.
+    canActivate: [authGuard],
+    loadChildren: () => import('./features/tabs/tabs.routes').then(m => m.routes)
+  }
+  // {
+  //   path: 'tabs',
+  //   loadChildren: () => import('./features/tabs/tabs.routes').then((m) => m.routes),
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: 'tabs',
+  //   pathMatch: 'full',
+  // },
 ];
